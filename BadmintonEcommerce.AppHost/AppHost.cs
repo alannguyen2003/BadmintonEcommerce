be75fd630d9edebecfae9f6295cc032cde3631cb
alannguyen2003 <nguyenho30112003@gmail.com>
2026-04-16 13:59:16 +0700

    setup nuget package

--- BadmintonEcommerce.AppHost/AppHost.cs
+++ BadmintonEcommerce.AppHost/AppHost.cs
@@ -1,5 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var minio = builder.AddMinIO("minio")
+    .WithEnvironment("MINIO_ROOT_USER", "admin")
+    .WithEnvironment("MINIO_ROOT_PASSWORD", "password");
 var api = builder.AddProject<Projects.BadmintonEcommerce_API>("api");
 
 builder.Build().Run();
