61263c07400a45053f943b8fb580436cb8d33ae7
alannguyen2003 <nguyenho30112003@gmail.com>
2026-04-29 10:23:24 +0700

    feat: add home page blazor application'

---
+++ BadmintonEcommerce.BlazorApplication/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace BadmintonEcommerce.BlazorApplication;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBlazorApplication(this IServiceCollection services, IConfiguration configuration)
+        => services
+            .AddApiHttpClient(configuration);
+
+    public static IServiceCollection AddApiHttpClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        var apiClient = configuration["Client:Api"] ?? string.Empty;
+        
+        return services;
+    }
+}
