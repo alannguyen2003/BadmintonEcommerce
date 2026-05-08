4da1cf07c53130deea7a57c7e423a9c9eec1fd92
alannguyen2003 <nguyenho30112003@gmail.com>
2026-05-08 14:03:56 +0700

    feat: add pagination for customer site

--- BadmintonEcommerce.BlazorApplication/Components/Pages/ShopPage.razor.cs
+++ BadmintonEcommerce.BlazorApplication/Components/Pages/ShopPage.razor.cs
@@ -1,4 +1,5 @@
 ﻿using BadmintonEcommerce.BlazorApplication.Abstraction.Services;
+using BadmintonEcommerce.BlazorApplication.Utils;
 using BadmintonEcommerce.Contracts.API.Presentation;
 using BadmintonEcommerce.Contracts.API.Presentation.Client.Category;
 using BadmintonEcommerce.Contracts.API.Presentation.Product.Responses;
@@ -10,20 +11,45 @@ public partial class ShopPage(IProductCategoryHttpService productCategoryHttpSer
     IProductHttpService productHttpService) : ComponentBase
 {
     private List<CategoryResponse> Categories { get; set; }
-
+    
+    [Parameter]
+    public Guid? CategoryId { get; set; }
+    
     public PagedRequest<Guid> PagedRequest { get; set; } = new()
     {
         PageNumber = 1,
-        PageSize = 20,
+        PageSize = 9,
         Data = Guid.NewGuid()
     };
+
+    private int FirstElementOfPage => Products != null ? (Products.PageNumber - 1) * Products.PageSize + 1 : 0;
+    private int LastElementOfPage => Products != null ? 
+        (Products.PageNumber * Products.PageSize) > Products.TotalCount ? Products.TotalCount : (Products.PageNumber * Products.PageSize)
+        : 0;
 
     public PagedList<List<ProductResponse>> Products { get; set; } = new PagedList<List<ProductResponse>>();
 
     protected override async Task OnInitializedAsync()
     {
         Categories = await productCategoryHttpService.GetClientCategories();
+        int currentPage = 0;
+        navigationManager.TryGetQueryString<int>("page", out currentPage);
+        PagedRequest.PageNumber = currentPage;
+        Console.WriteLine($"Current page: {PagedRequest.PageNumber}");
+        if (CategoryId != null) PagedRequest.Data = CategoryId.Value;
         Products = await productHttpService.GetProductsByCategoryAndDefault(PagedRequest);
-        Console.WriteLine(Products.Data.Count);
+        if (currentPage == 0) Products.PageNumber = 1;
+    }
+
+    private string GetAbsoluteUri()
+    {
+        var uri = new Uri(navigationManager.Uri);
+
+        return uri.AbsolutePath;
+    }
+
+    private string Paging(int page)
+    {
+        return GetAbsoluteUri() + "?page=" + page;
     }
 }
